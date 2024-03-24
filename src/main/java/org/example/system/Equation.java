d0b3c40c82c759bc0c23dd9d669101bc5bf11c81
Andryss <andr.krivosheev@gmail.com>
2024-03-24 18:34:09 +0300

    Impl Equation

--- src/main/java/org/example/system/Equation.java
+++ src/main/java/org/example/system/Equation.java
@@ -5,6 +5,8 @@ import org.example.log.LnCalc;
 import org.example.log.LogCalc;
 import org.example.trig.SecCalc;
 
+import static java.lang.Math.pow;
+
 @RequiredArgsConstructor
 public class Equation {
 
@@ -13,6 +15,13 @@ public class Equation {
     private final LnCalc lnCalc;
 
     public double calc(double x, double precision) {
-        return 0;
+        if (x <= 0) {
+            return secCalc.sec(x, precision);
+        } else {
+            // какая точность нужна, чтобы уложиться в precision ????
+            double lowPrecision = precision * 1e-3;
+            return pow(pow(pow(logCalc.log3(x, lowPrecision) + logCalc.lg(x, lowPrecision), 2), 3) + lnCalc.ln(x, lowPrecision), 2);
+        }
     }
+
 }

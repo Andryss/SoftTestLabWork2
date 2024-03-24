54d3a700a4a08cd909eb423f77ee16a87e581f33
Andryss <andr.krivosheev@gmail.com>
2024-03-24 18:31:04 +0300

    Impl CosCalc

--- src/main/java/org/example/trig/CosCalc.java
+++ src/main/java/org/example/trig/CosCalc.java
@@ -2,12 +2,22 @@ package org.example.trig;
 
 import lombok.RequiredArgsConstructor;
 
+import static java.lang.Math.sqrt;
+import static org.example.util.TrigUtil.ranged;
+
 @RequiredArgsConstructor
 public class CosCalc {
 
     private final SinCalc sinCalc;
 
     public double cos(double x, double precision) {
-        return 0;
+        if (precision <= 0) {
+            throw new IllegalArgumentException("Precision must be more than zero");
+        }
+        double sin = sinCalc.sin(x, precision * 1e-1);
+        double cos = sqrt(1 - sin * sin);
+        x = ranged(x);
+        return (x > -Math.PI/2 && x < Math.PI/2) ? cos : -cos;
     }
+
 }
